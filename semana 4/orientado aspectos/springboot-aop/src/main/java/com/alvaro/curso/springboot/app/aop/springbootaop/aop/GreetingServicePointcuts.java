db16dc5f36c1186d8f95fb419ea2d7e9d73cf549
Alvaro789 <apradop01@educantabria.es>
2024-04-23 09:48:33 +0200

    Orientado aspectos

---
+++ semana 4/orientado aspectos/springboot-aop/src/main/java/com/alvaro/curso/springboot/app/aop/springbootaop/aop/GreetingServicePointcuts.java
@@ -0,0 +1,20 @@
+package com.alvaro.curso.springboot.app.aop.springbootaop.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class GreetingServicePointcuts {
+
+    @Pointcut("execution(* com.alvaro.curso.springboot.app.aop.springbootaop.services.GreetingService.*(..))")
+    public void greetingLoggerPointCut(){
+
+    }
+
+    @Pointcut("execution(* com.alvaro.curso.springboot.app.aop.springbootaop.services.GreetingService.*(..))")
+    public void greetingFooLoggerPointCut(){
+
+ }
+}
