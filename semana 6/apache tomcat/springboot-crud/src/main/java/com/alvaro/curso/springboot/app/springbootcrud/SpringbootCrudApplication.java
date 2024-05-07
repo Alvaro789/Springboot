388abe8bcea697635dd4c4f73660ed221017ee91
Alvaro789 <apradop01@educantabria.es>
2024-05-07 09:43:14 +0200

    Apache tomcat

---
+++ semana 6/apache tomcat/springboot-crud/src/main/java/com/alvaro/curso/springboot/app/springbootcrud/SpringbootCrudApplication.java
@@ -0,0 +1,14 @@
+package com.alvaro.curso.springboot.app.springbootcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringbootCrudApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudApplication.class, args);
+	}
+
+}
---
+++ semana 6/apache tomcat/springboot-crud/src/main/java/com/alvaro/curso/springboot/app/springbootcrud/repositories/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.alvaro.curso.springboot.app.springbootcrud.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.alvaro.curso.springboot.app.springbootcrud.entities.Role;
+
+public interface RoleRepository extends CrudRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
+    
+}
+
