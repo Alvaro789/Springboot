4c646aea5c4e751e9d97612f9c4dea31474ac371
Alvaro789 <apradop01@educantabria.es>
2024-05-08 15:14:35 +0200

    backend

---
+++ semana 6/backend/backend-products/src/main/java/com/alvaro/curso/springboot/backend/backendproducts/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.alvaro.curso.springboot.backend.backendproducts.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.alvaro.curso.springboot.backend.backendproducts.entities.Product;
+
+@RepositoryRestResource(path = "products")
+public interface ProductRepository extends CrudRepository<Product, Long>{
+
+
+}
